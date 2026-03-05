10929767e0623bfbf65defe5ea3f11d4c74ac3d8
Kamil Tatrocki <kamil.tatrocki@oulook.com>
2026-03-05 19:22:56 +0100

    c

--- utils/calendar.py
+++ utils/calendar.py
@@ -5,9 +5,43 @@ class Calendar:
     def __init__(self):
         self.calendar_data = main_consumer.calendar
         self.calendar_exception = main_consumer.calendar_dates
+        self.trips_data = main_consumer.trips
+
+        self._day_cache = {}
     
-    def get_stops_in_proper_day(self, day: datetime):
+    def get_Active_service_id_for_day(self, day: datetime):
         """
-        return service_id list maybe? but than i will have to connect via trips.txt get route_id bcs stops have only route_id
+        return list service_id check if it is correct TODO vibecoded
         """
-        pass
+        day_val = int(day.strftime('%Y%m%d'))
+        day_name = day.strftime('%A').lower()
+        
+        regular = self.calendar_data[
+            (self.calendar_data['start_date'].astype(int) <= day_val) & 
+            (self.calendar_data['end_date'].astype(int) >= day_val) & 
+            (self.calendar_data[day_name] == 1)
+        ]['service_id'].tolist()
+        
+        exceptions = self.calendar_exception[self.calendar_exception['date'].astype(int) == day_val]
+        
+        added = exceptions[exceptions['exception_type'] == 1]['service_id'].tolist()
+        removed = exceptions[exceptions['exception_type'] == 2]['service_id'].tolist()
+        
+        active_services = set(regular + added) - set(removed)
+        
+        return list(active_services)
+    
+    def get_routes_for_services(self, service_ids: list):
+        active_routes = self.trips_data[
+            self.trips_data['service_id'].isin(service_ids)
+        ]['route_id'].unique().tolist()
+        
+        return active_routes
+
+    def get_all_active_routes_in_day(self, day: datetime):
+        active_services = self.get_Active_service_id_for_day(day)
+        return self.get_routes_for_services(active_services)
+    
+    def check_if_route_is_active_on_day(self, route_id: str, day: datetime):
+        active_routes = self.get_all_active_routes_in_day(day)
+        return route_id in active_routes
--- utils/stops.py
+++ utils/stops.py
@@ -1,12 +1,43 @@
 from ..data_consumer import main_consumer
+import math
+
 class Stops:
     def __init__(self):
         self.data = main_consumer.stops
 
-    def calculate_distance_beetwen_station(self):
-        pass
+    def calculate_distance_beetwen_station(self, stop_id):
+        """
+        return {"stop_id": distance}
+        """
+        stop = self.data[self.data['stop_id'] == stop_id]
+        if stop.empty:
+            raise ValueError(f"Stop with id {stop_id} not found.")
+        
+        stop_lat = stop['stop_lat'].values[0]
+        stop_lon = stop['stop_lon'].values[0]
 
-    def __calculate_distance_based_on_lan_lat(self):
-        #np. za pomoc  ̨a wzoru haversine lub aproksymacji euklide-sowej na rzutowanych współrz  ̨ednych
-        pass
+        res = {}
+        
+        for index, row in self.data.iterrows():
+            if row['stop_id'] != stop_id:
+                distance = self.__calculate_distance_based_on_lan_lat(stop_lat, row['stop_lat'], stop_lon, row['stop_lon'])
+                res[row['stop_id']] = distance
+                
+        return res
+
+    def __calculate_distance_based_on_lan_lat(self, lat1, lat2, lon1, lon2):
+        #aproksymacji euklidesowej na rzutowanych współrzednych
+        earth_radius = 6371000
+    
+        phi1 = math.radians(lat1)
+        phi2 = math.radians(lat2)
+        delta_phi = math.radians(lat2 - lat1)
+        delta_lambda = math.radians(lon2 - lon1)
+        
+        mean_phi = (phi1 + phi2) / 2
+        
+        x = delta_lambda * math.cos(mean_phi)
+        y = delta_phi
+        
+        return math.sqrt(x**2 + y**2) * earth_radius
     
