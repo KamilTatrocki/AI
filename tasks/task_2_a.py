60c464d19f65e0081967c5f0a933458cc6359c98
KamilTatrocki <kamil.tatrocki@outlook.com>
2026-03-15 14:53:18 +0100

    c

--- tasks/task_2_a.py
+++ tasks/task_2_a.py
@@ -33,7 +33,18 @@ def start():
     criteria= "p"
     start_time_str= "2026-03-08 8:00"
 
-    tabu_search = TabuSearch(A, L, criteria, start_time_str)
+
+    #ANOTHER SETTINGS
+    #modyfikacja algorytmu (a) ze zmiennym rozmiarem T, zale˙znym od długo´sci L, w celu minimalizacji funkcji kosztu (5 punktów)
+    is_dynamic_tabu_size_B = True
+
+    # modyfikacja algorytmu (a) z dodaniem kryterium aspiracji, w celu minimalizacji funkcji kosztu (5punktów)
+    is_aspiration_criterion_C = True
+
+    # modyfikacja algorytmu (a) z dodaniem strategii próbkowania s ˛asiedztwa, w celu minimalizacji funkcji kosztu i skrócenia czasu oblicze´n (10 punktów)
+    is_neighborhood_sampling_D = True
+
+    tabu_search = TabuSearch(A, L, criteria, start_time_str, is_dynamic_tabu_size_B, is_aspiration_criterion_C, is_neighborhood_sampling_D)
     tabu_search.search()
 
 if __name__ == "__main__":
